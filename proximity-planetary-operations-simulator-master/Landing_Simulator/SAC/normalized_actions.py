56508c6f6596ee0aa5d826f296337b6e76f19ed6
Silver0x10 <smanganar@gmail.com>
2023-12-17 20:20:52 +0100

    state space wrapper: missing return and normalization need to be checked

--- proximity-planetary-operations-simulator-master/Landing_Simulator/SAC/normalized_actions.py
+++ proximity-planetary-operations-simulator-master/Landing_Simulator/SAC/normalized_actions.py
@@ -7,8 +7,11 @@ class NormalizedActions(gym.ActionWrapper):
     low = self.action_space.low
     high = self.action_space.high
     
-    action = low + (action + 1) * 0.5 * (high - low)
-    action = np.clip(action, low, high)
+    # TODO fix normalization
+    # action = low + (action + 1) * 0.5 * (high - low)
+    # action = np.clip(action, low, high)
+    
+    return action
     
   def _revers_action(self, action):
     low = self.action_space.low
