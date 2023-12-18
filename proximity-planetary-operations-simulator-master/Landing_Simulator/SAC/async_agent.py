1ff745fc3549cbad170f97f61fad6d8e7674e5fc
Silver0x10 <smanganar@gmail.com>
2023-12-18 12:52:51 +0100

    async scheleton

---
+++ proximity-planetary-operations-simulator-master/Landing_Simulator/SAC/async_agent.py
@@ -0,0 +1,20 @@
+import torch
+
+class AsyncAgent(torch.multiprocessing.Process):
+  def __init__(self, id, global_episode_counter):
+    super(AsyncAgent, self).__init__()
+    self.id = id
+    self.global_episode_counter = global_episode_counter
+    # self.env = LanderGymEnv(renders=False)
+    print('Starting agent', self.id)
+  
+  def update(self):
+    pass
+  
+  def run(self):
+    print('Starting agent', self.id)
+    print('Agent', self.id, 'running episode', self.global_episode_counter.value)
+    self.update()
+    with self.global_episode_counter.get_lock():
+      self.global_episode_counter.value += 1
+    print('Agent', self.id, 'finished')
