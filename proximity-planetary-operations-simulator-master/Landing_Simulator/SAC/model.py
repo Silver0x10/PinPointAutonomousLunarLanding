e82116365a7be8080cec1f63d51adda371971b03
Silver0x10 <smanganar@gmail.com>
2023-12-18 17:21:27 +0100

    network model centralized

--- proximity-planetary-operations-simulator-master/Landing_Simulator/SAC/model.py
+++ proximity-planetary-operations-simulator-master/Landing_Simulator/SAC/model.py
@@ -1,6 +1,98 @@
 import torch
 import torch.nn as nn
 from torch.distributions import Normal
+import torch.optim as optim
+
+class NetworksManager(nn.Module):
+  def __init__(self, state_dim, action_dim, hidden_dim):
+    super(ValueNetwork, self).__init__()
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('Device set to : ' + str(torch.cuda.get_device_name(self.device)))
+
+    self.value_net = ValueNetwork( self.device, state_dim, hidden_dim).to(self.device)
+    self.target_value_net = ValueNetwork( self.device, state_dim, hidden_dim).to(self.device)
+    self.soft_q_net1 = SoftQNetwork( self.device, state_dim, action_dim, hidden_dim).to(self.device)
+    self.soft_q_net2 = SoftQNetwork( self.device, state_dim, action_dim, hidden_dim).to(self.device)
+    self.policy_net = PolicyNetwork( self.device, state_dim, action_dim, hidden_dim).to(self.device)
+  
+    for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+      target_param.data.copy_(param.data)
+
+    self.value_criterion = nn.MSELoss()
+    self.soft_q_criterion1 = nn.MSELoss()
+    self.soft_q_criterion2 = nn.MSELoss()
+  
+    value_lr = 3e-4
+    soft_q_lr = 3e-4
+    policy_lr = 3e-4
+
+    self.value_optimizer = optim.Adam(self.value_net.parameters(), lr=value_lr)
+    self.soft_q_optimizer1 = optim.Adam(self.soft_q_net1.parameters(), lr=soft_q_lr)
+    self.soft_q_optimizer2 = optim.Adam(self.soft_q_net2.parameters(), lr=soft_q_lr)
+    self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=policy_lr)
+  
+  def save(self, filename):
+    torch.save(self.value_net.state_dict(), filename + "_value_net")
+    torch.save(self.target_value_net.state_dict(), filename + "_target_value_net")
+    torch.save(self.soft_q_net1.state_dict(), filename + "_soft_q_net1")
+    torch.save(self.soft_q_net2.state_dict(), filename + "_soft_q_net2")
+    torch.save(self.policy_net.state_dict(), filename + "_policy_net")
+  
+  def load(self, filename):
+    self.value_net.load_state_dict(torch.load(filename + "_value_net"))
+    self.target_value_net.load_state_dict(torch.load(filename + "_target_value_net"))
+    self.soft_q_net1.load_state_dict(torch.load(filename + "_soft_q_net1"))
+    self.soft_q_net2.load_state_dict(torch.load(filename + "_soft_q_net2"))
+    self.policy_net.load_state_dict(torch.load(filename + "_policy_net"))
+    
+  def update(self, replay_buffer, batch_size, gamma=0.99, soft_tau=1e-2):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    state = torch.FloatTensor(state).to(self.device)
+    next_state = torch.FloatTensor(next_state).to(self.device)
+    action = torch.FloatTensor(action).to(self.device)
+    reward = torch.FloatTensor(reward).unsqueeze(1).to(self.device)
+    done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(self.device)
+
+    predicted_q_value1 = self.soft_q_net1(state, action)
+    predicted_q_value2 = self.soft_q_net2(state, action)
+    predicted_value = self.value_net(state)
+    new_action, log_prob, epsilon, mean, log_std = self.policy_net.evaluate(state)
+
+    #Q Function Training:
+    target_value = self.target_value_net(next_state)
+    target_q_value = reward + (1 - done) * gamma * target_value
+    q_value_loss1 = self.soft_q_criterion1(predicted_q_value1, target_q_value.detach())
+    q_value_loss2 = self.soft_q_criterion2(predicted_q_value2, target_q_value.detach())
+
+    self.soft_q_optimizer1.zero_grad()
+    q_value_loss1.backward()
+    self.soft_q_optimizer1.step()
+
+    self.soft_q_optimizer2.zero_grad()
+    q_value_loss2.backward()
+    self.soft_q_optimizer2.step()
+
+    #Value Function Training:    
+    predicted_new_q_value = torch.min(self.soft_q_net1(state, new_action), self.soft_q_net2(state, new_action))
+
+    target_value_func = predicted_new_q_value - log_prob 
+    #print(predicted_new_q_value.shape, log_prob.shape, predicted_value.shape, target_value_func.shape)
+
+    value_loss = self.value_criterion(predicted_value, target_value_func.detach())
+
+    self.value_optimizer.zero_grad()
+    value_loss.backward()
+    self.value_optimizer.step()
+    #Policy Function Training
+    policy_loss = (log_prob - predicted_new_q_value).mean()
+    self.policy_optimizer.zero_grad()
+    policy_loss.backward()
+    self.policy_optimizer.step()  
+
+    for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+      target_param.data.copy_(target_param.data * (1.0 - soft_tau) + param.data * soft_tau)
+
 
 class ValueNetwork(nn.Module):
   def __init__(self, device, state_dim, hidden_dim, init_w=3e-3):
